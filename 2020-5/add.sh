0aec0e7e79a228b73bc8366d4cc969e39c39f723
wolfGuidao <2351903911@qq.com>
2020-05-13 15:23:02 +0800

    add.sh

--- 2020-5/add.sh
+++ 2020-5/add.sh
@@ -2,6 +2,11 @@
 
 read -p "input your git add filename:" filename
 
+git add $(filename)
+git commit -a -m "$(filename)"
+git push
+
 expect{
-  
+ "Username for 'https://github.com':"{send "wolfGuidao\r",exp_continue}
+ "Password for 'https://wolfGuidao@github.com':"{send "lttfyt137126\r"}
 }
