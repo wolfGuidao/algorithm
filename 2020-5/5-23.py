81061024841085762c6cd94df92715c6d12fa2cc
wolfGuidao <2351903911@qq.com>
2020-05-20 16:49:25 +0800

    打家劫舍

--- 2020-5/5-23.py
+++ 2020-5/5-23.py
@@ -9,11 +9,22 @@
 #         return ret
 #     return dp(0)
 
-def rob(nums):
+# def rob(nums):
+#     dp_i_1 = 0
+#     dp_i_2 = 0
+#     dp_i_0 = 0
+#     for money in range(len(nums) - 1, -1, -1):
+#         dp_i_0 = max(dp_i_1, dp_i_2 + nums[money])
+#         dp_i_2 = dp_i_1
+#         dp_i_1 = dp_i_0
+#
+#     return dp_i_0
+
+def rob(nums, start, end):
     dp_i_1 = 0
     dp_i_2 = 0
     dp_i_0 = 0
-    for money in range(len(nums) - 1, -1, -1):
+    for money in range(end, start - 1, -1):
         dp_i_0 = max(dp_i_1, dp_i_2 + nums[money])
         dp_i_2 = dp_i_1
         dp_i_1 = dp_i_0
@@ -21,5 +32,6 @@ def rob(nums):
     return dp_i_0
 
 if __name__ == "__main__":
-    list_nums = [2,7,9,3,1]
-    print(rob(list_nums))
+    list_nums = [2,3,2]
+    print(max(rob(list_nums, 0, len(list_nums) - 2), rob(list_nums, 1, len(list_nums) - 1)))
+
