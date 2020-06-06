f0927817640fff26c0f9f7c6e28cc86d736b7e51
wolfGuidao <2351903911@qq.com>
2020-06-06 18:38:23 +0800

    顺时针

--- 2020-6/6-18.cpp
+++ 2020-6/6-18.cpp
@@ -7,19 +7,65 @@
 
 #include "Header.hpp"
 
+class Solution {
+public:
+	vector<int> printMatrix(vector<vector<int> > matrix) {
+		vector<int>ret;
+		int m = matrix.size();
+		int n = matrix[0].size();
+		int num = m * n;
+		int x = 0;
+		int y = 0;
+		//ret.push_back(matrix[0][0]);
+		while (num  > 0)
+		{
+			while (y + 1 < n&&matrix[x][y + 1] != 0)
+			{
+				ret.push_back(matrix[x][y]);
+				matrix[x][y] = 0;
+				y++;
+				num--;
+			}
+			while (x + 1 < m&&matrix[x + 1][y] != 0)
+			{
+				ret.push_back(matrix[x][y]);
+				matrix[x][y] = 0;
+				x++;
+				num--;
+			}
+			while (y - 1 >= 0 && matrix[x][y - 1] != 0)
+			{
+				ret.push_back(matrix[x][y]);
+				matrix[x][y] = 0;
+				y--;
+				num--;
+			}
+			while (x - 1 >= 0 && matrix[x - 1][y] != 0)
+			{
+				ret.push_back(matrix[x][y]);
+				matrix[x][y] = 0;
+				x--;
+				num--;
+			}
+			if (num == 1)
+			{
+				ret.push_back(matrix[x][y]);
+				num--;
+			}
+		}
+		return ret;
+	}
+};
+
 int main()
 {
-    string str = "hello world i am wolf";
-    size_t len = str.size();
-    for(size_t i = 0;i < len;i++)
-    {
-      if(str[i] == ' ')
-      {
-        str.replace(i,0,"%20");
-        i += 2;
-      }
-    }
-    cout<<str<<endl;
-    return 0;
-}
+  vector<vector<int>> arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+  
+  int count = 9;
+  
+  while(count)
+  {
 
+  }
+  return 0;
+}
