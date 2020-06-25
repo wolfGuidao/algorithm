578243ece6dba1520ea589c9e974f939268f1211
wolfGuidao <2351903911@qq.com>
2020-06-25 18:24:40 +0800

    *?

--- 2020-7/7-3.cpp
+++ 2020-7/7-3.cpp
@@ -131,3 +131,61 @@ int main()
     cout<<(sum == num)<<endl;
     return 0;
 }
+
+/*
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size(), m = p.size();
+        vector< vector<bool> > dp(n+1, vector<bool>(m+1, false)); 
+            dp[0][0] = true;
+
+        // initialize 
+        for (int i = 1; i <= m; ++ i)
+        {
+            if(p[i - 1] == '*' && dp[0][i - 1]) 
+                dp[0][i] = dp[0][i - 1];
+        }
+
+        for (int i = 1; i <= n; ++ i)
+        {
+            for (int j = 1; j <= m; ++ j)
+            {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
+                {
+                    dp[i][j] = dp[i - 1][j - 1];// ismatch, move on
+                }
+                else if (p[j - 1] == '*')
+                {
+                    bool zero, mul;// '*' act as zero, '*' act as multiple characters
+                    zero = (j < m && s[i - 1] == p[j] && dp[i - 1][j - 1]) || dp[i][j - 1];
+                    mul = dp[i - 1][j];
+                    dp[i][j] = zero || mul;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
+*/
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int i = 0, j = 0, iStar = -1, jStar = -1, m = s.size(), n = p.size();
+        while (i < m) {
+            if (j < n && (s[i] == p[j] || p[j] == '?')) {
+                ++i, ++j;
+            } else if (j < n && p[j] == '*') {
+                iStar = i;
+                jStar = j++;
+            } else if (iStar >= 0) {
+                i = ++iStar;
+                j = jStar + 1;
+            } else return false;
+        }
+        while (j < n && p[j] == '*') ++j;//去除多余星号
+        return j == n;
+    }
+};
+
