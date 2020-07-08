1ce264cf5251e931bb479b93b9aa74d88f952b54
wolfGuidao <2351903911@qq.com>
2020-07-08 21:08:29 +0800

    test

--- 2020-7/7-25.cpp
+++ 2020-7/7-25.cpp
@@ -7,9 +7,64 @@
 
 #include "Header.hpp"
 
+static int a = 999;
+
+void Func()
+{
+    static int b;
+    cout<<b<<endl;
+}
+
+class A 
+{
+    public:
+        A()
+        {
+
+        }
+
+    static void GetB(A aa)
+    {
+        cout<<aa.b<<endl;
+    }
+    public:
+        static int a;
+        int b = 666;
+};
+int A::a = 999;
+
+void TestDeque()
+{
+    //deque<int> deq;
+    list<int> deq;
+    deq.push_back(1);
+    deq.push_back(2);
+    deq.push_back(3);
+    deq.push_back(4);
+    deq.push_back(5);
+    cout<<deq.size()<<endl;
+
+    auto it = deq.begin();
+    while(it != deq.end())
+    {
+        //it = deq.erase(it);
+        deq.erase(it++);
+    }
+    cout<<deq.size()<<endl;
+}
+
 int main()
 {
-
+    //static int a;
+    //for(auto i = 0;i < 10;i++)
+    //{
+    //    a += i;
+    //    cout<<a<<endl;
+    //}
+    //cout<<a<<endl;
+    //Func();
+    
+    TestDeque();
     return 0;
 }
 
