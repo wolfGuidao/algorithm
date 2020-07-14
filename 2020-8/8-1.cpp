e178032122a3265e25fd3d68209809538f0b4dec
wolfGuidao <2351903911@qq.com>
2020-07-14 21:59:16 +0800

    test

---
+++ 2020-8/8-1.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+  > File Name: 8-1.cpp
+  > Author: wolf
+  > Mail: wolfguidao@163.com 
+  > Created Time: 2020年07月13日 星期一 19时46分02秒
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 0;
+    int j = 0;
+	int arr[9] ;
+	for (; i <= 9; i++) {
+		arr[i] = 0;
+		printf("hello world\n");
+	}
+	return 0;
+}
+
