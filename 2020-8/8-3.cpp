8b9cb5ecbdcc7d20588d3c1c634e1a41769b9d1e
wolfGuidao <2351903911@qq.com>
2020-07-16 23:04:42 +0800

    分段翻转连表

--- 2020-8/8-3.cpp
+++ 2020-8/8-3.cpp
@@ -3,7 +3,7 @@
 # Author: wolf
 # Mail: wolfguidao@163.com 
 # Created Time: 2020年07月15日 星期三 19时33分22秒
-*************************************************************************/
+ *************************************************************************/
 
 //评测题目: 按段（段内的元素不翻转）翻转链表：如链表 1->2->3->4->5->6->7->8->9，如果段大小为3，翻转后为7->8->9->4->5->6->1->2->3。注意段大小作为参数传入。要求编写可以运行的测试用例。
 #include <iostream>
@@ -12,159 +12,194 @@ using namespace std;
 
 struct Node 
 {
-  	Node(int data)
-      :_data(data)
-        ,next(nullptr)
-        {}
-  
-	int _data;
-	Node* next;
+    Node(int data)
+        :_data(data)
+         ,next(nullptr)
+    {}
+
+    int _data;
+    Node* next;
 };
 
-//翻转整个链表
+//翻转整个链表 OK 
 Node* ReverseChild(Node*& root)
 {
-	if(root->next == nullptr)
+    if(root->next == nullptr)
     {
-    	return root;
+        return root;
     }
-  
-  	Node* last = ReverseChild(root->next);
-  	root->next->next = root;
-  	root->next = nullptr;
-    
-  	return last;
+
+    Node* last = ReverseChild(root->next);
+    root->next->next = root;
+    root->next = nullptr;
+
+    return last;
 }
 
-//翻转start和end区间内的链表
-void ReverseOfKChild(Node*& start,Node*& end)
+//翻转start和end区间内的链表 OK 
+void ReverseOfKChild(Node* start,Node* end)
 {
-  	if(start == nullptr)
+    if(start == nullptr)
     {
-      	return ;
+        return ;
     }
-  
-  	Node* pre = nullptr;
-  
-  	while(start != nullptr)
+
+    //保存上一个节点
+    Node* pre = nullptr;
+
+    //保存下一个节点
+    Node* Next = nullptr;
+    while(start != nullptr)
     {
-      	Node* Next = start->next;
-      
-      	start->next = pre;
-      
-      	pre = start;
-      
-      	if(Next == nullptr || start == end)
+        //if的作用防止空指针的问题
+        if(Next != end)
+            Next = start->next;
+
+        //把下一个节点和上一个节点连接起来
+        start->next = pre;
+
+        //更新上一个结点的位置
+        pre = start;
+
+        //代表start指针已经走到末尾，可以直接结束循环
+        if(Next == nullptr || start == end)
         {
-          	break;
+            break;
         }
-      
-      	start = Next;
+
+        //把start放到提前保存的Next处
+        start = Next;
     }
-  
-  return ;
+
+    return ;
 }
 
+
+#if 1
 //翻转K个一翻转
 Node* ReverseOfK(Node* root,int k)
 {
-  	if(k == 0)
+    if(k == 0)
     {
-      	return root;
+        return root;
     }
-  
-  	Node* fast = root;
-  	Node* pre = nullptr;
-  	Node* head = nullptr;
-  
-  	while(fast != nullptr)
+
+    //保存记录每个K段的末尾处
+    Node* fast = root;
+
+    //这个变量的作用是非常非常非常非常重要的，他是把每个K段的连表连接起来
+    Node* pre = nullptr;
+
+    //翻转后的头节点
+    Node* head = nullptr;
+
+    //循环遍历
+    while(fast != nullptr)
     {
-      	int t = k;
-      	Node* slow = fast;
-      	while(t > 1)
+        int t = k;
+        //记录每个K段的开始位置
+        Node* slow = fast;
+
+        //找到每个K段的末尾位置
+        while(t > 1)
         {
-          	if(fast->next == nullptr)
+            if(fast->next == nullptr)
             {
-              	break;
+                break;
             }
-          
-          	fast = fast->next;
-          	
-          	t--;
+
+            fast = fast->next;
+
+            t--;
         }
-      
-      	if(head == nullptr) 
+
+        //保存头结点
+        if(head == nullptr) 
         {
-          	head = fast;
+            head = fast;
         }
-      
-      	Node* temp = fast->next;
-      	
-      	ReverseOfKChild(slow,fast);
-      
-      	slow->next = temp;
-      
-      	if(pre != nullptr)
+
+        //记录每个K段末尾节点的下一个节点，因为一旦前面的发生翻转，那么如果不保存就找不到了
+        Node* temp = fast->next;
+
+        //翻转slow和fast区间的节点，该该函数一定不要引用传参，因为在内部会改变slow
+        ReverseOfKChild(slow,fast);
+
+        //把每个K段的开始位置（经过翻转后是每个K段的结束位置）和提前保存的temp相连，把相邻的K段节点构成一个链 
+        slow->next = temp;
+
+        if(pre != nullptr)
         {
-          	pre->next = fast;
+            pre->next = fast;
         }
-      
-      	pre = slow;
-      	fast = temp;
+
+        //记录上一个slow的位置
+        pre = slow;
+
+        //把fast放到提前保存的节点处
+        fast = temp;
     }
-  return head;
+    return head;
 }
+#endif
+
 
 //本题的入口函数
 Node* Reverse(Node*& root,int k)
 {
-	if(root == nullptr)
+    if(root == nullptr)
     {
-    	return nullptr;
+        return nullptr;
     }
-  
-  	//9->8->7....->1
-  	//7->8->9....->1->2->3
-  	Node* last = ReverseChild(root);
-  
-  	Node* head = ReverseOfK(last,k);
-  
-  	return head;
+
+    //9->8->7....->1
+    //7->8->9....->1->2->3
+    Node* last = ReverseChild(root);
+
+    Node* head = ReverseOfK(last,k);
+
+    return head;
 }
 
 void Print(Node* root)
 {
-  	if(root)
+    Node* cur = root;
+    while(cur)
     {
-      	printf("%d->\n",root->_data);
-      	root = root->next;
+        printf("%d->\n",cur->_data);
+        cur = cur->next;
     }
 }
 
 int main()
 {
-  	vector<int> arr {1,2,3,4,5,6,7,8,9};
-  	
-  	Node* root = nullptr;
-  	Node* tail = nullptr;
-  	for(size_t i = 0;i < arr.size();i++)
+    vector<int> arr {1,2,3,4,5,6,7,8,9,10,11};
+
+    Node* root = nullptr;
+    Node* tail = nullptr;
+    for(size_t i = 0;i < arr.size();i++)
     {
-      	if(root == nullptr)
+        if(root == nullptr)
         {
-          	root = new Node(arr[i]);
-          	tail = root;
+            root = new Node(arr[i]);
+            tail = root;
+            continue;
         }
-      
-      	Node* temp = new Node(arr[i]);
-      	tail->next = temp;
-      	tail = tail->next;
+
+        Node* temp = new Node(arr[i]);
+        tail->next = temp;
+        tail = tail->next;
     }
-  
-  	int k = 3;
-  
-  	Print(root);
-  	Node* head = Reverse(root,k);
-  	Print(head);
-  	
-  	return 0;
+
+    int k = 3;
+
+    Print(root);
+    cout<<endl;
+    Node* head = Reverse(root,k);
+    //Node* head = ReverseChild(root);
+    //ReverseOfKChild(root,tail);
+    //Node* head = ReverseOfK(root,k);
+    Print(head);
+
+    return 0;
 }
