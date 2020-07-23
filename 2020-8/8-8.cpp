e53f90f242b0a441b64919483b25af0d259fb32e
wolfGuidao <2351903911@qq.com>
2020-07-23 15:50:40 +0800

    test

--- 2020-8/8-8.cpp
+++ 2020-8/8-8.cpp
@@ -47,6 +47,74 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    int numSubmat(vector<vector<int>>& mat) {
+        if(mat.empty())
+        {
+            return 0;
+        }
+
+        int m = (int)mat.size();
+        int n = (int)mat[0].size();
+
+        //dp[i][j]代表以i，j坐标为右端点，向左延续连续1的个数。意思是在第i行，j为终点，从j
+        //向左连续1的个数，注意是连续连续连续
+        vector<vector<int>> dp(m,vector<int>(n));
+
+        for(int i = 0;i < m;i++)
+        {
+            for(int j = 0;j < n;j++)
+            {
+                //处理边界
+                if(j == 0)
+                {
+                    dp[i][j] = mat[i][j];
+                }
+                //如果当前位置为1，就代表可以在左边的基础上进行+1
+                else if(mat[i][j] == 1)
+                {
+                    dp[i][j] = dp[i][j - 1] + 1;
+                }
+                //如果当前位置为0，那么dp代表的是连续1的个数，中间断了，直接赋值为0
+                else 
+                {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        //保存最后的结果
+        int sum = 0;
+
+        //遍历，每次求以i，j坐标为右下角
+        for(int i = 0;i < m;i++)
+        {
+            for(int j = 0;j < n;j++)
+            {
+                //保存点i，j坐标处向左延续连续1的个数
+                int num = dp[i][j];
+
+                //向上枚举每一行
+                for(int k = i;k >= 0;k--)
+                {
+                    //一旦某一行为0，那么求min后都为0，代表没有构成矩阵
+                    if(num == 0)
+                    {
+                        break;
+                    }
+                    //更新结果
+                    num = min(num,dp[k][j]);
+                    sum += num;
+                }
+            }
+        }
+        return sum;
+    }
+};
+
+
 int main()
 {
     cout<<Solution().numSub("111")<<endl;;
