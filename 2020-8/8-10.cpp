e22849c2c8bb5b167eb848565aa56491ed71b45e
wolfGuidao <2351903911@qq.com>
2020-07-24 16:45:26 +0800

    test

--- 2020-8/8-10.cpp
+++ 2020-8/8-10.cpp
@@ -69,3 +69,121 @@ int main()
     cout<<que.top()._val<<endl;
     return 0;
 }
+
+/*
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if(dividend == 0)
+        {
+            return 0;
+        }
+
+        if(divisor == 1)
+        {
+            return dividend;
+        }
+
+        if(divisor == -1)
+        {
+            if(dividend == INT_MIN)
+            {
+                return INT_MAX;
+            }
+            return -dividend;
+        }
+
+        int flag = 1;
+
+        if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0))
+        {
+            flag = 0;
+        }
+
+        dividend = abs(dividend);
+        divisor = abs(divisor);
+
+        long long ret = div(dividend,divisor);
+
+        if(flag)
+        {
+            return ret > INT_MAX ? INT_MAX : ret;
+        }
+        else 
+        {
+            return -ret;
+        }
+    }
+
+    long long div(int a,int b)
+    {
+        if(a < b)
+        {
+            return 0;
+        }
+
+        long long count = 1;
+        long long temp_b = b;
+
+        while(a >= (temp_b + temp_b))
+        {
+            temp_b = temp_b + temp_b;
+            count = count + count;
+        }
+
+        return count + div(a - temp_b,b);
+    }
+};
+*/
+
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if(dividend == 0) 
+            return 0;
+        
+        if(divisor == 1) 
+            return dividend;
+        
+        if(divisor == -1)
+        {
+            if(dividend > INT_MIN) 
+                return -dividend;
+            return INT_MAX;
+        }
+
+        long a = dividend;
+        long b = divisor;
+        int sign = 1; 
+        if((a > 0 && b < 0) || (a < 0 && b > 0))
+        {
+            sign = -1;
+        }
+
+        a = abs(a);
+        b = abs(b);
+
+        long res = div(a,b);
+
+        if(sign > 0)
+            return res > INT_MAX ? INT_MAX : res;
+        return -res;
+    }
+    int div(long a, long b)
+    {  
+        if(a < b) 
+            return 0;
+        
+        long count = 1;
+        long tb = b; 
+        while((tb + tb) <= a)
+        {
+            count = count + count;
+            tb = tb + tb;
+        }
+        return count + div(a-tb,b);
+    }
+};
+
+
