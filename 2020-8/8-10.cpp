86bbc5e0c17a4964f2410ce8b3f41baf4c096346
wolfGuidao <2351903911@qq.com>
2020-07-24 19:08:04 +0800

    test

--- 2020-8/8-10.cpp
+++ 2020-8/8-10.cpp
@@ -3,7 +3,7 @@
 # Author: wolf
 # Mail: wolfguidao@163.com 
 # Created Time: 2020年07月24日 星期五 15时04分34秒
-*************************************************************************/
+ *************************************************************************/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,10 +11,10 @@ using namespace std;
 class cmp 
 {
     public:
-    bool operator()(int l,int r)
-    {
-        return l > r;
-    }
+        bool operator()(int l,int r)
+        {
+            return l > r;
+        }
 };
 
 int main()
@@ -71,119 +71,169 @@ int main()
 }
 
 /*
+   class Solution {
+   public:
+   int divide(int dividend, int divisor) {
+   if(dividend == 0)
+   {
+   return 0;
+   }
+
+   if(divisor == 1)
+   {
+   return dividend;
+   }
+
+   if(divisor == -1)
+   {
+   if(dividend == INT_MIN)
+   {
+   return INT_MAX;
+   }
+   return -dividend;
+   }
+
+   int flag = 1;
+
+   if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0))
+   {
+   flag = 0;
+   }
+
+   dividend = abs(dividend);
+   divisor = abs(divisor);
+
+   long long ret = div(dividend,divisor);
+
+   if(flag)
+   {
+   return ret > INT_MAX ? INT_MAX : ret;
+   }
+   else 
+   {
+   return -ret;
+   }
+   }
+
+   long long div(int a,int b)
+   {
+   if(a < b)
+   {
+   return 0;
+   }
+
+   long long count = 1;
+   long long temp_b = b;
+
+   while(a >= (temp_b + temp_b))
+   {
+   temp_b = temp_b + temp_b;
+   count = count + count;
+   }
+
+   return count + div(a - temp_b,b);
+   }
+   };
+   */
+
+
 class Solution {
-public:
-    int divide(int dividend, int divisor) {
-        if(dividend == 0)
-        {
-            return 0;
-        }
+    public:
+        int divide(int dividend, int divisor) {
+            if(dividend == 0) 
+                return 0;
 
-        if(divisor == 1)
-        {
-            return dividend;
-        }
+            if(divisor == 1) 
+                return dividend;
 
-        if(divisor == -1)
-        {
-            if(dividend == INT_MIN)
+            if(divisor == -1)
             {
+                if(dividend > INT_MIN) 
+                    return -dividend;
                 return INT_MAX;
             }
-            return -dividend;
+
+            long a = dividend;
+            long b = divisor;
+            int sign = 1; 
+            if((a > 0 && b < 0) || (a < 0 && b > 0))
+            {
+                sign = -1;
+            }
+
+            a = abs(a);
+            b = abs(b);
+
+            long res = div(a,b);
+
+            if(sign > 0)
+                return res > INT_MAX ? INT_MAX : res;
+            return -res;
         }
+        int div(long a, long b)
+        {  
+            if(a < b) 
+                return 0;
 
-        int flag = 1;
-
-        if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0))
-        {
-            flag = 0;
+            long count = 1;
+            long tb = b; 
+            while((tb + tb) <= a)
+            {
+                count = count + count;
+                tb = tb + tb;
+            }
+            return count + div(a-tb,b);
         }
+};
 
-        dividend = abs(dividend);
-        divisor = abs(divisor);
+#include <bits/stdc++.h>
+using namespace std;
 
-        long long ret = div(dividend,divisor);
-
-        if(flag)
-        {
-            return ret > INT_MAX ? INT_MAX : ret;
-        }
-        else 
-        {
-            return -ret;
-        }
-    }
-
-    long long div(int a,int b)
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> num(n + 1);
+    for(int i = 1; i <= n; i++)
     {
-        if(a < b)
-        {
-            return 0;
-        }
-
-        long long count = 1;
-        long long temp_b = b;
-
-        while(a >= (temp_b + temp_b))
-        {
-            temp_b = temp_b + temp_b;
-            count = count + count;
-        }
-
-        return count + div(a - temp_b,b);
+        cin>>num[i];
     }
-};
-*/
 
+    int K;
+    int D;
+    cin>>K>>D;
 
-class Solution {
-public:
-    int divide(int dividend, int divisor) {
-        if(dividend == 0) 
-            return 0;
-        
-        if(divisor == 1) 
-            return dividend;
-        
-        if(divisor == -1)
-        {
-            if(dividend > INT_MIN) 
-                return -dividend;
-            return INT_MAX;
-        }
+    vector<vector<long>> dpm(K + 1,vector<long>(n + 1));//dpm[i][j]表示选中了i个人，以第j个人结尾的能力最大乘积
+    vector<vector<long>> dpn(K + 1,vector<long>(n + 1));;//dpn[i][j]表示选中了i个人，以第j个人结尾的能力最小乘积
 
-        long a = dividend;
-        long b = divisor;
-        int sign = 1; 
-        if((a > 0 && b < 0) || (a < 0 && b > 0))
-        {
-            sign = -1;
-        }
-
-        a = abs(a);
-        b = abs(b);
-
-        long res = div(a,b);
-
-        if(sign > 0)
-            return res > INT_MAX ? INT_MAX : res;
-        return -res;
+    for(int j = 1; j < n + 1; j++)
+    {//初始
+        dpm[1][j] = num[j];
+        dpn[1][j] = num[j];
     }
-    int div(long a, long b)
-    {  
-        if(a < b) 
-            return 0;
-        
-        long count = 1;
-        long tb = b; 
-        while((tb + tb) <= a)
-        {
-            count = count + count;
-            tb = tb + tb;
-        }
-        return count + div(a-tb,b);
+
+    for(int i = 1; i < K + 1; i++)
+    {//初始
+        dpm[i][1] = num[1];
+        dpn[i][1] = num[1];
     }
-};
 
-
+    for(int i = 2;i < K + 1;i++)
+    {
+        for(int j = 2;j < n + 1;j++)
+        {
+            for(int k = max(1,j - D);k < j;k++)
+            {
+                dpm[i][j] = max(dpm[i][j],max(dpm[i - 1][k] * num[j],dpn[i - 1][k] * num[j]));
+                dpn[i][j] = min(dpn[i][j],min(dpm[i - 1][k] * num[j],dpn[i - 1][k] * num[j]));
+            }
+        }
+    }
+    long max_num = max(dpm[K][1],dpn[K][1]);
+    for(int j = 2;j < n + 1;j++)
+    {
+        max_num = max(max(dpm[K][j],dpn[K][j]),max_num);
+    }
+    cout<<max_num<<endl;
+    return 0;
+}
