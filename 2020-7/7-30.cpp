46f678b8265da6812a891d53684ee5763e130431
wolfGuidao <2351903911@qq.com>
2020-08-07 19:17:15 +0800

    test

--- 2020-7/7-30.cpp
+++ 2020-7/7-30.cpp
@@ -87,6 +87,7 @@ public:
 };
 #endif 
 
+#if 0
 class A
 {
     public:
@@ -209,3 +210,154 @@ public:
         dfs(root->right,root);
     }
 };
+#endif 
+
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    vector<int> arr {1,-1,2,4,-5,7,-9};
+    for(auto e : arr)
+    {
+        cout<<e<<" ";
+    }
+    cout<<endl;
+
+    sort(arr.begin(),arr.end(),[](int a,int b)
+            {
+                return abs(a) < abs(b);
+            });
+
+    for(auto e : arr)
+    {
+        cout<<e<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+
+/*
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& A) 
+    {
+        if(A.empty())
+        {
+            return true;
+        }
+
+        vector<bool> flag(A.size(),false);
+        sort(A.begin(),A.end(),[](int a,int b)
+        {
+            return abs(a) < abs(b);
+        });
+
+        unordered_map<int,int> index_map;
+        //1 2 4 4 8 16
+        //1 : 0
+        //2 : 1
+        //4 : 3
+        //8 : 4
+        //16 : 5
+        bool ZERO = true;
+        for(int i = 0;i < A.size();i++)
+        {
+            if(A[i] != 0)
+            {
+                ZERO = false;
+            }
+            index_map[A[i]] = i;
+        }
+
+        if(ZERO)
+        {
+            return true;
+        }
+
+        int count = 0;
+        for(int i = 0;i < A.size();i++)
+        {
+            //1 2 4 4 8 16
+            //1 : 0
+            //2 : 1
+            //4 : 3
+            //8 : 4
+            //16 : 5
+            
+            if(flag[i])
+            {
+                continue;
+            }
+            
+            auto it = index_map.find(2 * A[i]);
+            if(it == index_map.end())
+            {
+                return false;
+            }
+
+            int index = it->second;
+
+            if(flag[index] == true)
+            {
+                return false;
+            }
+            //cout<<A[i]<<" : "<<A[index]<<endl;
+            flag[i] = true;
+            flag[index] = true;
+            count++;
+        }
+
+        if(count != A.size() / 2)
+        {
+            return false;
+        }
+        return true;
+    }
+};
+*/
+
+
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& A) {
+        map<int,int>m_map;
+        for(auto i = 0;i < A.size();++i)
+            m_map[A[i]]++;
+        
+        auto ite = m_map.begin();
+        bool judge = true;
+        while(ite != m_map.end())
+        {
+            if(ite->first < 0 && ite->second != 0)
+            {
+                m_map[(ite->first) / 2] -= ite->second;
+                if(m_map[(ite->first) / 2] < 0)
+                {
+                    judge=false;
+                    break;
+                }
+            }
+           else if(ite->first == 0)
+           {
+                if(ite->second % 2 != 0)
+                {
+                   judge = false;
+                   break;
+                }
+           }
+           else if(ite->first > 0 && ite->second != 0)
+           {
+               m_map[(ite->first) * 2] -= ite->second;
+                if(m_map[(ite->first) * 2] < 0)
+                {
+                   judge = false;
+                   break;
+                }
+           }
+           ite++; 
+        }
+        return judge;
+    }
+};
+
