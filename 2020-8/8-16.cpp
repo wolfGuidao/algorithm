ea1f6123c13ebca9fd6cda715c8277d63ed83c50
wolfGuidao <2351903911@qq.com>
2020-08-09 23:34:16 +0800

    test

--- 2020-8/8-16.cpp
+++ 2020-8/8-16.cpp
@@ -66,6 +66,61 @@ public:
     }
 };
 
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        if(root == nullptr)
+        {
+            return {};
+        }
+
+        vector<vector<int>> ret;
+        queue<Node*> que;
+        que.push(root);
+        while(!que.empty())
+        {
+            vector<int> temp;
+            int Size = que.size();
+            for(int i = 0;i < Size;i++)
+            {
+                Node* node = que.front();
+                que.pop();
+
+                temp.push_back(node->val);
+
+                for(auto e : node->children)
+                {
+                    if(e)
+                        que.push(e);
+                }
+            }
+            ret.push_back(temp);
+        }
+        return ret;
+    }
+};
+
 int main()
 {
     set<int> arr {1,2,3,4,5,6,7};
