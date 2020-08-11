40b159ce043e73f1e8589f92a70ab4c4e8ea0832
wolfGuidao <2351903911@qq.com>
2020-08-11 09:37:11 +0800

    test

--- 2020-8/8-16.cpp
+++ 2020-8/8-16.cpp
@@ -193,6 +193,28 @@ public:
     }
 };
 
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<int> dp(amount + 1,0);
+
+        //base case:
+        dp[0] = 1;
+
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = 1; j <= amount; j++)
+            {
+                if (j - coins[i] >= 0)
+                    dp[j] = dp[j] + dp[j - coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+};
+
+
 int main()
 {
     set<int> arr {1,2,3,4,5,6,7};
