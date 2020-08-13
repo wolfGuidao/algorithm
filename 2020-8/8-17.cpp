b155831db9c8045a3b85a6a79549c75bbbe46b98
wolfGuidao <2351903911@qq.com>
2020-08-13 23:38:38 +0800

    test

--- 2020-8/8-17.cpp
+++ 2020-8/8-17.cpp
@@ -115,6 +115,29 @@ public:
     }
 };
 
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = (int)matrix.size();
+
+        //(n + 1) / 2为总的循环次数，也就是层数，每一圈旋转完毕后矩阵缩小一圈，
+        // + 1的目的是向上取整
+        for (int i = 0; i < (n + 1) / 2; i ++) 
+        {
+            //j可以理解为add
+            for (int j = 0; j < n / 2; j++) 
+            {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+};
+
+
 int main()
 {
     int sock = socket(AF_INET,SOCK_STREAM,0);
