6a55a4bda30cc45e927f2b36f015b0bb348ac282
wolfGuidao <2351903911@qq.com>
2020-08-14 18:56:27 +0800

    test

--- 2020-8/8-17.cpp
+++ 2020-8/8-17.cpp
@@ -415,6 +415,66 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.empty() || matrix[0].empty())
+        {
+            return false;
+        }   
+
+        /*
+        for(int i = 0;i < matrix.size();i++)
+        {
+            if(target > matrix[i][matrix[i].size() - 1])
+            {
+                continue;
+            }
+
+            for(int j = matrix[0].size() - 1;j >= 0;j--)
+            {
+                if(target == matrix[i][j])
+                {
+                    return true;
+                }
+            }
+
+            break;
+        }
+        return false;
+        */
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int left = 0;
+        int right = m * n - 1;
+
+        while(left <= right)
+        {
+            int mid = (left + right) >> 1;
+
+            int mid_index_x = mid / n;
+            int mid_index_y = mid % n;
+
+            if(matrix[mid_index_x][mid_index_y] == target)
+            {
+                return true;
+            }
+            else if(matrix[mid_index_x][mid_index_y] > target)
+            {
+                right = mid - 1;
+            }
+            else 
+            {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+};
+
 int main()
 {
     int sock = socket(AF_INET,SOCK_STREAM,0);
