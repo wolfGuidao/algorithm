3c9c1d44831dddb7b819b3ee43e9460d476f8840
wolfGuidao <2351903911@qq.com>
2020-08-16 22:04:36 +0800

    test

--- 2020-8/8-18.cpp
+++ 2020-8/8-18.cpp
@@ -3,10 +3,13 @@
 # Author: wolf
 # Mail: wolfguidao@163.com 
 # Created Time: 2020年08月16日 星期日 15时25分34秒
-*************************************************************************/
+ *************************************************************************/
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
+#if 0
 class A 
 {
     public:
@@ -15,11 +18,62 @@ class A
             cout<<"~A()"<<endl;
         }
 };
+#endif 
+
+class KMP
+{
+    public:
+        KMP(const string str)
+            :_str(str)
+        {
+            _next.resize(str.size() + 2);
+            init();
+        }
+
+        void search(const string pat) 
+        {
+            int n = pat.size();
+            int m = _str.size();
+
+            int j = 0; //j表示当前模版串的待匹配位置
+            for(int i = 0; i < n; ++i) 
+            {
+                while(j && pat[i] != _str[j]) 
+                    j = _next[j]; //不停的转移，直到可以匹配或者走到0
+                
+                if(pat[i] == _str[j]) 
+                    j++; //如果相等，模版串中待匹配位置可以移一位了。
+
+                if(j == m) 
+                    printf("%d\n", i - m + 1);
+            }
+        }
+
+
+    private:
+        void init() 
+        {
+            int n = _str.size();
+            _next[0] = _next[1] = 0;
+            for(int i = 1; i < n; ++i) 
+            {
+                int j = _next[i];
+                while(j && _str[i] != _str[j]) 
+                    j = _next[j]; //一旦回到1，表明窗口大小为0了，只能回到最初的字符
+
+                _next[i + 1] = _str[i] == _str[j] ? j + 1 :0;
+            }
+        }
+
+        string _str;
+        vector<int> _next;
+};
 
 int main()
 {
-    A* p = nullptr;
-    p = new A[4];
-    delete p;
+    string str = "aabbaabbddaabbeeff";
+    string pat = "aabbee";
+    KMP kmp(str);
+    kmp.search(pat);
     return 0;
 }
