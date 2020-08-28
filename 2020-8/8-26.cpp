c361c735a16a46c06f0861810b941628adf03b34
wolfGuidao <2351903911@qq.com>
2020-08-28 15:11:09 +0800

    test

--- 2020-8/8-26.cpp
+++ 2020-8/8-26.cpp
@@ -57,3 +57,45 @@ public:
 
     }
 };
+
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int n1 = version1.size();
+        int n2 = version2.size();
+        
+        int i = 0;
+        int j = 0;
+        while(i < n1 || j < n2)
+        {
+            int index1 = i;
+            int index2 = j;
+            while(index1 < n1 && version1[index1] != '.')
+            {
+                index1++;
+            }
+
+            while(index2 < n2 && version2[index2] != '.')
+            {
+                index2++;
+            }
+
+            int num1 = (index1 == i ? 0 : stoi(version1.substr(i,index1 - i)));
+            int num2 = (index2 == j ? 0 : stoi(version2.substr(j,index2 - j)));
+            if(num1 > num2)
+            {
+                return 1;
+            }
+            else if(num1 < num2)
+            {
+                return -1;
+            }
+            i = index1 + 1;
+            j = index2 + 1;
+        }
+
+        return 0;
+    }
+};
+
+
