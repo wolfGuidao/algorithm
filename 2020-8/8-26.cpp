64c8b286975dc94ddd9e10784ed3b485b6ba23f8
wolfGuidao <2351903911@qq.com>
2020-08-30 12:53:17 +0800

    test

--- 2020-8/8-26.cpp
+++ 2020-8/8-26.cpp
@@ -242,3 +242,78 @@ public:
     }
 };
 
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> GetNext(string& match)
+{
+    vector<int> next(match.size() + 1,0);
+    next[0] = -1;
+    
+    int i = 2;
+    int k = 0;
+    while(i < match.size())
+    {
+        if(match[i - 1] == match[k])
+        {
+            next[i] = k + 1;
+            k = next[i];
+            i++;
+        }
+        else if(k == -1)
+        {
+            next[i] = 0;
+            i++;
+        }
+        else 
+        {
+            k = next[k];
+        }
+    }
+    return next;
+}
+
+int main()
+{
+    string str;
+    string match;
+    while(getline(cin,str))
+    {
+        getline(cin,match);
+        vector<int> next = GetNext(match);
+        
+        int flag = 1;
+        
+        int i = 0;
+        int j = 0;
+        while(i < str.size())
+        {
+            if(j == -1 || str[i] == match[j])
+            {
+                i++;
+                j++;
+                if(j == match.size())
+                {
+                    flag = 0;
+                    cout<<i - match.size()<<" ";
+                    j = next[j];
+                }
+            }
+            else
+            {
+                j = next[j];
+                if(j == -1)
+                {
+                    j = 0;
+                    i++;
+                }
+            }
+        }
+        if(flag)
+            cout<<-1<<endl;
+    }
+    return 0;
+}
