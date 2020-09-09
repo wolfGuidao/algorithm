c3745f9aa3341d6974c0c16a64093eafb7a4476e
wolfGuidao <2351903911@qq.com>
2020-09-09 20:16:51 +0800

    test

--- 2020-9/9-8.cpp
+++ 2020-9/9-8.cpp
@@ -183,3 +183,23 @@ public:
     }
 };
 
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        int begin = 0;
+        for(int i = 0;i < arr2.size();i++)
+        {
+            for(int j = 0;j < arr1.size();j++)
+            {
+                if(arr1[j] == arr2[i])
+                {
+                    swap(arr1[j],arr1[begin]);
+                    begin++;
+                }
+            }
+        }
+
+        sort(arr1.begin() + begin,arr1.end());
+        return arr1;
+    }
+};
