389445677f7446cb82560a2d1c41b270f85c37f2
wolfGuidao <2351903911@qq.com>
2020-09-16 11:11:59 +0800

    test

--- 2020-9/9-13.cpp
+++ 2020-9/9-13.cpp
@@ -205,3 +205,40 @@ public:
         return ret;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(root == nullptr)
+        {
+            return ;
+        }
+
+        flatten(root->left);
+        flatten(root->right);
+
+        TreeNode* Left = root->left;
+        TreeNode* Right = root->right;
+
+        root->left = nullptr;
+        root->right = Left;
+
+        TreeNode* cur = root;
+        while(cur->right)
+        {
+            cur = cur->right;
+        }
+        cur->right = Right;
+    }
+};
