3a9284f80eb481d1971bf7a828ff0100cf99e1d5
wolfGuidao <2351903911@qq.com>
2020-09-16 11:22:31 +0800

    test

--- 2020-9/9-13.cpp
+++ 2020-9/9-13.cpp
@@ -242,3 +242,54 @@ public:
         cur->right = Right;
     }
 };
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    void connectChild(Node* node1,Node* node2)
+    {
+        if(node1 == nullptr || node2 == nullptr)
+        {
+            return ;
+        }
+
+        //链接节点
+        node1->next = node2;
+
+        //链接同一个父亲节点的左右孩子
+        connectChild(node1->left,node1->right);
+        connectChild(node2->left,node2->right);
+
+        //链接不同父亲孩子
+        connectChild(node1->right,node2->left);
+    }
+
+    Node* connect(Node* root) {
+        if(root == nullptr)
+        {
+            return root;
+        }   
+
+        connectChild(root->left,root->right);
+
+        return root;
+    }
+};
